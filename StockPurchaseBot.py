3e2e8c5e0ccc42569c2951de3807089bf9a2fe7d
RoyM <roymusafi@gmail.com>
2022-05-11 22:02:07 +0300

    Changing the wallet to be a global paramter over all files

--- StockPurchaseBot.py
+++ StockPurchaseBot.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from config import stockList,wallet
 
 
 class StockPurchaseBot:
@@ -6,6 +7,22 @@ class StockPurchaseBot:
         self.st = st
 
     def display(self):
-        self.st.title("Stock Purchase Bot")
-        self.st.write('Please choose a stock: ')
-        # TODO: Add a stock
+        self.st.title("Stock Trading Bot")
+        with self.st.form('Stock Watchlist Form'):
+            ticker = self.st.text_input('Please enter a stock ticker: ')
+            amount = self.st.number_input('Please enter the amount of stocks you would like to purchase: ', min_value=1, step=1 )
+            submitted = self.st.form_submit_button("Submit")
+            if submitted:
+                if amount > wallet.getBalance():
+                    self.st.error('Not Enough Money')
+                wallet.withdraw(amount)
+                stockList[ticker] = amount
+
+        with self.st.expander('Watch watchlist'):
+            i = 0
+            cols = self.st.columns(3)
+            for stock in stockList:
+                if i >= 3:
+                    i = 0
+                cols[i].metric(stock, stockList[stock])
+                i = i+1
---
+++ venv/config.py
@@ -0,0 +1,6 @@
+from WalletBalance import WalletBalance
+# Defining the parameters used by many different files
+
+wallet = WalletBalance(0)
+stockList = {}
+portfolioStocks = 0
