4b32a3cca06463fe84dcda2b702b216f4eb12637
RoyM <roymusafi@gmail.com>
2022-05-16 20:02:42 +0300

    Final Changes

--- PortfolioPage.py
+++ PortfolioPage.py
@@ -1,6 +1,39 @@
 import streamlit as st
-from config import stockList
-from LinearRegressionAlgo import get_pred_table
+from config import stockList, portfolioStocks
+from LinearRegressionAlgo import get_lr_prediction
+from HomePage import load_data
+import yfinance as yf
+
+
+def get_market_price(ticker):
+    return yf.Ticker(ticker).info['regularMarketPrice']
+
+
+def get_amount(stock, amountofStock):
+    return get_market_price(stock) * amountofStock
+
+
+def get_portfolioValue():
+    sum = 0
+    for stock in portfolioStocks.keys():
+        sum += get_amount(stock, portfolioStocks[stock][0])
+    return sum
+
+
+def BuyOrHold():
+    for stock in stockList.copy():
+        if get_market_price(stock) < get_lr_prediction(1, load_data(stock)):
+            amount = stockList.pop(stock)
+            if stock in portfolioStocks:
+                portfolioStocks[stock][0] += get_shares(amount, stock)
+                portfolioStocks[stock][1] += amount
+            else:
+                portfolioStocks[stock] = [get_shares(amount, stock), amount]
+
+
+def get_shares(amount, ticker):
+    return amount / get_market_price(ticker)
+
 
 class PortfolioPage:
     def __init__(self, st):
@@ -8,3 +41,14 @@ class PortfolioPage:
 
     def display(self):
         self.st.title("Portfolio")
+        with self.st.spinner('Loading...'):
+            if stockList:
+                BuyOrHold()
+            self.st.write('Your Portfolio Value: ', round(get_portfolioValue(), 2), '$')
+            i = 0
+            cols = self.st.columns(3)
+            for stock in portfolioStocks:
+                if i >= 3:
+                    i = 0
+                cols[i].metric(stock, round(portfolioStocks[stock][0], 2), round(((get_amount(stock, portfolioStocks[stock][0]) - portfolioStocks[stock][1])/portfolioStocks[stock][1])*100, 2))
+                i = i + 1
--- StockPurchaseBot.py
+++ StockPurchaseBot.py
@@ -3,12 +3,6 @@ from config import stockList, wallet
 import yfinance as yf
 
 
-def get_amount_to_pay(amountOfStock, ticker):
-    stock = yf.Ticker(ticker)
-    price = stock.info['regularMarketPrice'] * amountOfStock
-    return price
-
-
 class StockPurchaseBot:
     def __init__(self, st):
         self.st = st
@@ -18,18 +12,16 @@ class StockPurchaseBot:
         self.st.write('Your current balance is ', wallet.getBalance(), '$')
         ticker = self.st.text_input('Please enter a stock ticker: ')
         if ticker:
-            with st.spinner('Loading...'):
-                self.st.write('The Current Price is: ', get_amount_to_pay(1, ticker), '$')
             with self.st.form('Stock Watchlist Form'):
                 amount = self.st.number_input('Please enter the amount of  you would like to deposit: ',
-                                                     min_value=0.0)
+                                              min_value=0.0)
                 submitted = self.st.form_submit_button("Submit")
                 if submitted:
                     if amount > wallet.getBalance():
                         self.st.error('Not Enough Money')
                     else:
                         self.st.balloons()
-                        self.st.success('Success...')
+                        self.st.success('Success, now let the bot handle the work while you do other stuff :)')
                         wallet.withdraw(amount)
                         if ticker in stockList:
                             stockList[ticker] += amount
